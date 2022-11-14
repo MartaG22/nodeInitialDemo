1d5781d0fbdec45349f7caa4dca0da7608ddb3be
M G <martaxgarijo@gmail.com>
2022-11-14 01:08:10 +0100

    No havia gravat

--- app/appSocket.js
+++ app/appSocket.js
@@ -2,6 +2,7 @@
 // https://socket.io/get-started/chat
 // https://ull-esit-dsi-1617.github.io/estudiar-las-rutas-en-expressjs-alejandro-raul-35l2/rutasexpressjs.html#using-middleware
 // https://www.oscarblancarteblog.com/2018/01/16/implementar-json-web-tokens-nodejs/
+// https://carlosazaustre.es/autenticacion-con-token-en-node-js
 //! implementar jsonwebtoken en nodejs
 
 const express = require('express');
