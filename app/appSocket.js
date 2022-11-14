0b9411c89ba440a7c561add27c9d4bd6d580fc91
M G <martaxgarijo@gmail.com>
2022-11-14 01:07:28 +0100

    Agafant nota dels tutorials del JWT

--- app/appSocket.js
+++ app/appSocket.js
@@ -1,5 +1,8 @@
 // https://www.youtube.com/watch?v=p-OevzBqHyQ
 // https://socket.io/get-started/chat
+// https://ull-esit-dsi-1617.github.io/estudiar-las-rutas-en-expressjs-alejandro-raul-35l2/rutasexpressjs.html#using-middleware
+// https://www.oscarblancarteblog.com/2018/01/16/implementar-json-web-tokens-nodejs/
+//! implementar jsonwebtoken en nodejs
 
 const express = require('express');
 const app = express();
