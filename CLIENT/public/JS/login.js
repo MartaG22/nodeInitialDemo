e6c5827e8cc98e9862f7881b6e4248f80adf3fd9
M G <martaxgarijo@gmail.com>
2022-12-23 23:32:32 +0100

    Em passen les dades des del Front al Back, però no aconsegueixo que passi a la finestra del XAT

--- CLIENT/public/JS/login.js
+++ CLIENT/public/JS/login.js
@@ -3,7 +3,7 @@
 // https://www.youtube.com/watch?v=l8QD6BE24FU
 // document.querySelector('form').addEventListener("submit", (e) => {
 //     e.preventDefault();
-document.getElementById("btnLogin").addEventListener("submit", )
+// document.getElementById("btnLogin").addEventListener("submit", )
     function login() {
         let user = document.getElementById("usuario").value;
         let pass = document.getElementById("pass").value;
@@ -19,21 +19,21 @@ document.getElementById("btnLogin").addEventListener("submit", )
         })
         .then(res => res.json())
         .then(data => {
-            // console.log(data);
-            // let usuario = "Marta";
+            console.log(data);
+            let usuario = "Marta";
 
-            // let password = "1234";
-            // if(user == usuario && pass == password) {
-            //     alert ("Bienvenido al sistema: " + usuario);
-            //     // require('../HTML/xat.html')
-            //     window.location.assign('../HTML/xat.html');
-            //             // ENVIAR DATOS  VALIDADOS  INTRODUCIDOS AL BACK
+            let password = "1234";
+            if(user == usuario && pass == password) {
+                console.log ("Bienvenido al sistema: " + usuario);
+                // require('../HTML/xat.html')
+                // window.location.assign('../HTML/xat.html');
+                        // ENVIAR DATOS  VALIDADOS  INTRODUCIDOS AL BACK
         
-            // } else if (user == "" || pass == "") {
-            //     alert ("Por favor, introduce los datos requeridos");
-            // } else {
-            //     alert ("Usuario y/o contraseña no válidos");
-            // }
+            } else if (user == "" || pass == "") {
+                console.log ("Por favor, introduce los datos requeridos");
+            } else {
+                console.log ("Usuario y/o contraseña no válidos");
+            }
         }).catch(error => 
             console.log(error.message))
             // document.getElementById('login-error').innerHTML = error.message);
