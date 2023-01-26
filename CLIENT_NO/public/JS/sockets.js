f5576c20c45ba91b524832d1329447955aad20eb
M G <martaxgarijo@gmail.com>
2023-01-26 12:44:01 +0100

    Esborrant les carpetes antigues

--- CLIENT_NO/public/JS/sockets.js
+++
@@ -1,6 +0,0 @@
-const socket = io('http://localhost:3000', {
-    reconnectionDelayMax: 10000,
-    query: {
-        'token': sessionStorage.token
-    }
-});
--- SERVER_NO/app/controllers/message/newMessageController.js
+++
@@ -1,5 +0,0 @@
-const newMessage = () => {
-      const newMessageUser = document.getElementById('newMessage').value;
-      console.log(newMessageUser);
-      
-}
